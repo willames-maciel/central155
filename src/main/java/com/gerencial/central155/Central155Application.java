900204d4aec7d12975e7252daf693ea152ddd9d3
Willames Maciel <willamesmb@gmail.com>
2023-11-17 16:38:22 -0300

    Correções de configurações padrão

--- src/main/java/com/gerencial/central155/Central155Application.java
+++ src/main/java/com/gerencial/central155/Central155Application.java
@@ -3,11 +3,17 @@ package com.gerencial.central155;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class Central155Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Central155Application.class, args);
+        Locale.setDefault(Locale.US);
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Fortaleza"));
+
+        SpringApplication.run(Central155Application.class, args);
 	}
 
 }
